890b133c7052e9df30ea3b6260c8dab922434219
zhon <zhon@xputah.org>
2013-02-08 10:44:56 -0700

    implemnt slide directive imparatively

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,14 +4,38 @@
 
 
 angular.module('myApp.directives', [])
-    .directive('slides', function() {
+    .directive('slide', function() {
+
+        var wrapContent = function (content) {
+            return '<img src=' + encodeURI( content ) + ' />';
+        };
+
         return {
-            restrict: 'A',
-            template: 
-                ' <section ng-repeat="slide in slides">' +
-                '    <section data-autoslide=1000 ng-repeat="image in slide.images">' +
-                '        <img ng-src="{{image}}" />' +
-                '    </section>' +
-                '</section>'
+            restrict: 'A'
+            ,link: function (scope, element, attrs, controller) {
+                // resorting to imparative jQuery way
+                if (scope.slide.images.length == 1) {
+                    element.html(
+                        wrapContent(scope.slide.images[0])
+                    );
+                } else {
+                    var sections = '';
+                    for (var i=0,len=scope.slide.images.length; i < len; ++i) {
+                        sections +=
+                            '<section ' +
+                            function () {
+                                var result = '';
+                                if (i !== len-1) {
+                                    result = 'data-autoslide="1000" ';
+                                }
+                                return result;
+                            }() +
+                            '>' +
+                            wrapContent(scope.slide.images[i]) +
+                            '</section>';
+                    }
+                    element.html(sections);
+                }
+            }
         }
-     });
+    });
