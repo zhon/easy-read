64f017de98803cd70f9f1d06867bbbb237fa59c6
zhon <zhon@xputah.org>
2013-02-12 11:27:13 -0700

    + Added sound to new-sound-d

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,35 +7,39 @@ angular.module('myApp.directives', [])
             return '<img src=' + encodeURI( content ) + ' />';
         };
 
+        var audioContent = function (scope) {
+            var content = '';
+
+            if (scope.slide.audio) {
+                content += '<audio preload="auto" controls >';
+                content += '<source src="';
+                content += scope.slide.audio;
+                content += '" type="audio/mpeg" />';
+                content += '</audio>';
+            }
+            return content;
+        }
+
         return {
             restrict: 'A'
             ,link: function (scope, element, attrs, controller) {
-                var content = '';
-
-                if (scope.slide.audio) {
-                    content += '<audio preload="auto" controls >';
-                    content += '<source src="';
-                    content += scope.slide.audio;
-                    content += '" type="audio/mpeg" />';
-                    content += '</audio>';
-                }
+                var content = ''
                 // resorting to imparative jQuery way
                 if (scope.slide.images.length == 1) {
+                    content += audioContent(scope);
                     content += wrapContent(scope.slide.images[0])
                 } else {
                     for (var i=0,len=scope.slide.images.length; i < len; ++i) {
-                        content +=
-                            '<section ' +
-                            function () {
-                                var result = '';
-                                if (i !== len-1) {
-                                    result = 'data-autoslide="1000" ';
-                                }
-                                return result;
-                            }() +
-                            '>' +
-                            wrapContent(scope.slide.images[i]) +
-                            '</section>';
+                        content += '<section ';
+                        if (i !== len-1) {
+                            content += 'data-autoslide="1500" ';
+                        }
+                        content += '>';
+                        if (i == 0) {
+                            content += audioContent(scope);
+                        }
+                        content += wrapContent(scope.slide.images[i]);
+                        content += '</section>';
                     }
                 }
                 element.html(content);
