58b0a99f5effe12d115e4db4f19f6ef12bfb8ef6
zhon <zhon@xputah.org>
2013-02-14 07:49:31 -0700

    rb tool for converting fles

---
+++ scripts/convert.rb
@@ -0,0 +1,7 @@
+# take an image directory name
+# take the new name
+# take the sound filename and new sound filename
+#
+# creat a json file based on the name ontaining all the filenames imbedded
+#   including the first sound
+#
